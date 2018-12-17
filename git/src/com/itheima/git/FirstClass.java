827f1e7bc1f13510a5902bbbc5fa5dd048d23b0a
Entad <271464003@qq.com>
2018-12-17 20:55:51 +0800

    把本地工程添加到git

---
+++ git/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
